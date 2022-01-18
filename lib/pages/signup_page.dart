02e6e2afbf5f6ca383473ccfe4492221ea8acf7a
nishanbhat <rananishanbhat@gmail.com>
2022-01-18 15:32:48 +0545

    form validation for signUp page added

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatefulWidget {
@@ -8,6 +10,8 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  bool vis = true;
+  final _globalKey= GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,63 +25,76 @@ class _SignUpPageState extends State<SignUpPage> {
             tileMode: TileMode.repeated,
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Sign Up with Email",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2,
+        child: Form(
+          key: _globalKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Sign Up with Email",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            usernameTextField(),
-            emailTextField(),
-            passwordTextField(),
-            SizedBox(
+              SizedBox(
                 height: 20,
               ),
-
-               Container(
-                        width: 150,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.black87
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Sign Up",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
+              usernameTextField(),
+              emailTextField(),
+              passwordTextField(),
+              SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                onTap: () {
+                  if(_globalKey.currentState!.validate())
+                  {
+                    //send data to the rest erver
+                    // ignore: avoid_print
+                    print("validated");
+                  }
+                },
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.black87),
+                  child: Center(
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
-
-          ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-
-
 Widget usernameTextField() {
   return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text("Username"),
           TextFormField(
             // controller: _usernameController,
+            validator: (value) {
+              if (value!.isEmpty) return "Username can't be empty";
+              return null;
+            },
             decoration: InputDecoration(
               // errorText: validate ? null : errorText,
               focusedBorder: UnderlineInputBorder(
@@ -92,15 +109,20 @@ Widget usernameTextField() {
       ));
 }
 
-
 Widget emailTextField() {
   return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text("Email"),
           TextFormField(
             // controller: _usernameController,
+            validator: (value) {
+              if (value!.isEmpty) return "Email can't be empty";
+              if (!value.contains("@")) return "Email is Invalid";
+              return null;
+            },
             decoration: InputDecoration(
               // errorText: validate ? null : errorText,
               focusedBorder: UnderlineInputBorder(
@@ -116,15 +138,32 @@ Widget emailTextField() {
 }
 
 Widget passwordTextField() {
+  bool vis = true;
   return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Password"),
+          Text(
+            "Password",
+          ),
           TextFormField(
-            // controller: _usernameController,
+            obscureText: vis,
+            validator: (value) {
+              if (value!.isEmpty) return "Password can't be empty";
+              if (value.length <= 8) return "Password length must have <=8";
+              return null;
+            },
             decoration: InputDecoration(
-              // errorText: validate ? null : errorText,
+              suffixIcon: IconButton(
+                // ignore: dead_code
+                icon: Icon(vis ? Icons.visibility_off : Icons.visibility),
+                onPressed: () {
+                  setState(() {
+                    vis = !vis;
+                  });
+                },
+              ),
               helperText: "Password length should not exceed 8 characters",
               helperStyle: TextStyle(
                 fontSize: 10,
@@ -141,3 +180,4 @@ Widget passwordTextField() {
       ));
 }
 
+void setState(Null Function() param0) {}
