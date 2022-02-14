fd8416bb79bd9c5c9f0234bb4de29f23e483c7ed
nishanbhat <rananishanbhat@gmail.com>
2022-02-14 10:14:23 +0545

    main profile section added

--- lib/profile/MainProfile.dart
+++ lib/profile/MainProfile.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:blog_app/models/ProfileModel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
